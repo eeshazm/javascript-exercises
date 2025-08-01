5ce5214ee30e390203c7adf37a260cd4a66ffa3d
eeshazm <118532203+eeshazm@users.noreply.github.com>
2025-07-31 17:30:04 -0700

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // Loop version
+    // length = str.length;
+    // let reversed = '';
+    // for (let i = str.length - 1; i >= 0 ; i--) {
+    //     reversed += str[i];
+    // }
+    // return reversed;
+    
+    //Using built-in functions
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
