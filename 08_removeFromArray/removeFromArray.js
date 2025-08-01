85e32a886f83272c7ed72bdbc9d258cca47f4e1c
eeshazm <118532203+eeshazm@users.noreply.github.com>
2025-07-31 18:15:22 -0700

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    newArr = [];
+    arr.forEach((item) => {
+        if (!values.includes(item)){
+            newArr.push(item);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
