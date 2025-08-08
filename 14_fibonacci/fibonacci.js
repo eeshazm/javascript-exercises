1da4cbc8960639e7616dd20ad3420ca50c314e27
eeshazm <118532203+eeshazm@users.noreply.github.com>
2025-08-08 00:36:49 -0700

    Complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {  // Check for negative input
+        return "OOPS";
+    }
+    if (typeof num !== 'number') {  // Convert string input to number
+        num = parseInt(num);
+    }
+    if (num === 0)
+            return 0;
+    if (num === 1 || num === 2)
+            return 1;   
+    let fibonacci = [1,1];
+    for (let i = 2; i < num; i++) {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
 
+    return fibonacci[num-1];
 };
 
 // Do not edit below this line
