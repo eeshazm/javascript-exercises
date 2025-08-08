87fb1fcac45ecd4fc8a662ba1fda593459bf7edf
eeshazm <118532203+eeshazm@users.noreply.github.com>
2025-08-07 21:00:30 -0700

    Complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    left = 0;
+    right = str.length -1;
+    str = str.toLowerCase();
+    while (left < right) {
+        if (str[left] === ' ' || str[left] === ',' || str[left] === '.' || str[left] === '!' || str[left] === '?') {
+            left++;
+        } else if (str[right] === ' ' || str[right] === ',' || str[right] === '.' || str[right] === '!' || str[right] === '?') {
+            right--;
+        } else if (str[left] !== str[right]) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+    return true;
+    
 };
 
 // Do not edit below this line
